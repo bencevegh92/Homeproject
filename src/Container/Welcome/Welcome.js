b92daa9d57d275251a6f53ed48835a3f6873ef48
Bence Vegh <bence_vegh@epam.com>
2019-05-27 14:47:04 +0200

    Initial Commit

---
+++ src/Container/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Welcome.scss';
+
+const welcome = () => {
+    return (
+        <div className={classes.Welcome}>
+            <h1>Hali</h1>
+        </div>
+    );
+}
+
+export default welcome;
