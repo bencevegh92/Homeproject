046acc914933dbd472bb48e3a32ca8b38d433559
Bence Vegh <bence_vegh@epam.com>
2019-05-27 16:50:11 +0200

    Second commit

--- src/Container/Welcome/Welcome.js
+++ src/Container/Welcome/Welcome.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import classes from './Welcome.scss';
+import classes from './Welcome.module.scss';
 
 const welcome = () => {
     return (
         <div className={classes.Welcome}>
-            <h1>Hali</h1>
+            <h1 className={classes.Welcome__header}>Csepergő</h1>
+            <p className={classes.Welcome__paragraph}>Bébiszitter kereső</p>
+            <button className={classes.Welcome__button}>Get started</button>
         </div>
     );
 }
